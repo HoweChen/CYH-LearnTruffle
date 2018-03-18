de0e3abb25ae953317670f6f0098464d7f8dc33f
SkyFather <cyh278889789@gmail.com>
2018-03-18 22:09:19 +0800

    update with some truffle code

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,6 +2,9 @@ pragma solidity ^0.4.4;
 
 contract HelloWorld {
   function sayHello() returns (string) {
-    return ("Hello World");
+    return "Hello World";
+  }
+  function echo(string name) public view returns (string) {
+    return name;
   }
 }
